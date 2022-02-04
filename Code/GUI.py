d9aea66565ff6e73ea0933206d6a16ccde85089b
kozeygit <ko3lyonbrooks@gmail.com>
2022-02-04 08:27:46 +0000

    toolbar

--- Code/GUI.py
+++ Code/GUI.py
@@ -2,47 +2,32 @@
 import kivy
 kivy.require('1.0.1')
 from kivy.app import App
-from kivy.uix.relativelayout import RelativeLayout
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.image import Image
 from kivy.config import Config
 from kivy.core.window import Window
 from kivy.graphics import Color, Line, Rectangle, RoundedRectangle
-from kivy.uix.button import Button
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+class MainWindow(Widget):
+    pass
+
 
 class LogicGate(Widget):
     def __init__(self, **kwargs):
         pass
 
+class MyLayout(Widget):
+    pass
 
-class MyGridLayout(Widget):
-    name = ObjectProperty(None)
-    pizza = ObjectProperty(None)
-    colour = ObjectProperty(None)
+kv = Builder.load_file("test.kv")
 
-    def press(self):
-        name = self.name.text
-        pizza = self.pizza.text
-        colour = self.colour.text
-
-        #self.top_grid.add_widget(Label(text=f"Hello {name}, you like {pizza} pizza!"))
-        print(f"Hello {name}, you like {pizza} pizza!")
-
-        self.name.text = ""
-        self.pizza.text = ""
-        self.colour.text = ""
-
-
-
-class TestApp(App):
+class TheApp(App):
     def build(self):
-        return FloatLayout()
+        return MainWindow()
 
 
 if __name__ == '__main__':
-    TestApp().run()
+    TheApp().run()
