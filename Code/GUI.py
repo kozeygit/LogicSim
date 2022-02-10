a81911cf135ddb63c114ba6e635bc9e549113e56
kozeygit <ko3lyonbrooks@gmail.com>
2022-02-10 08:23:22 +0000

    10/02/2022

--- Code/GUI.py
+++ Code/GUI.py
@@ -10,27 +10,34 @@ from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.popup import Popup
 
 class MainWindow(Widget):
+    pass
+
+class ExitPopup(Popup):
     def closeWindow(self):
         sys.exit()
+
+class Gate(Widget):
+    pass
 
 class GateButton(Widget):
     pass
 
 class LogicGate(Widget):
-    def __init__(self, **kwargs):
-        pass
+    pass
 
 class MyLayout(Widget):
     pass
 
 kv = Builder.load_file("test.kv")
 
-class TheApp(App):
+class LogicGateSimulator(App):
     def build(self):
+        #self.icon = "Images/Me.jpg"
         return MainWindow()
 
 
 if __name__ == '__main__':
-    TheApp().run()
+    LogicGateSimulator().run()
