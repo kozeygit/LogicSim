d9f09a97a9d4e8a7d8d47624f70d4f51c5b7a6b7
kozeygit <ko3lyonbrooks@gmail.com>
2022-02-14 08:21:42 +0000

    Add gates to board - not finished

--- Code/GUI.py
+++ Code/GUI.py
@@ -7,11 +7,12 @@ from kivy.config import Config
 from kivy.core.window import Window
 from kivy.graphics import Color, Line, Rectangle, RoundedRectangle
 from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, NumericProperty, BooleanProperty
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.popup import Popup
 from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
 
 class MainWindow(Widget):
     pass
@@ -20,12 +21,33 @@ class ExitPopup(Popup):
     def closeWindow(self):
         sys.exit()
 
-class Gate(Scatter):
+class Gate(Button):
     def __init__(self, **kwargs):
         super(Gate, self).__init__(**kwargs)
 
-class GateButton(Widget):
-    pass
+class GateCanvas(FloatLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+#     def on_touch_down(self, touch):
+# # if the touch is not for me, and if i don't want to use it, avoid it.
+#         if not self.collide_point(*touch.pos):
+#             return
+#         print("WOOOO")
+#         self.root.ids[Board].addGate()
+
+
+    def addGate(self):
+        img = Image(source="Images/GateIcons/and.png")
+        self.add_widget(img)
+
+class GateButton(Button):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def addGateToBoard(self):
+        print("Hello")
+        self.root.ids["Board"].addGate()
 
 class LogicGate(Widget):
     pass
@@ -39,7 +61,6 @@ class LogicGateSimulator(App):
     def build(self):
         #self.icon = "Images/Me.jpg"
         return MainWindow()
-
 
 if __name__ == '__main__':
     LogicGateSimulator().run()
